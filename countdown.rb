567839e48ae963576b01d30b814277382938d57a
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-12 21:28:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,9 @@
 #write your code here
 
-def countdown(number)
-  count = number
-  while count > 0
+def countdown(number) 
+  while number > 0
     puts "#{number} SECOND(S)!"
-    count -= 1
+    number -= 1
   end
    "HAPPY NEW YEAR!"
 end
